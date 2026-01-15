38c9304e3f6cdcfaaf045756c2c9fda8b4c7a10a
khwazabani <khwazabanis@gmail.com>
2026-01-16 00:45:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,19 +6,3 @@ variable name{
   default="terraform-resource-bani"
   type=string
 }
-variable "subscription_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-  sensitive = true
-}
