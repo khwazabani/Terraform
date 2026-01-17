50cde57d4df8f33d133615caafdeba742e8b91b6
khwazabani <khwazabanis@gmail.com>
2026-01-17 14:43:08 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "jenkins-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_B2ls_v2"
   admin_username      = "azureuser"
 
   network_interface_ids = [
